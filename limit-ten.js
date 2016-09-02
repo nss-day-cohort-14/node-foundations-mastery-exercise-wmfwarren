9393219e826cdd38f6a0835084481b3f421151e9
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-02 13:42:29 -0500

    commented some

--- limit-ten.js
+++ limit-ten.js
@@ -1,7 +1,9 @@
 const {Transform} = require("stream");
 
-const MAX_WORDS = 10;
-let counter = 0;
+const MAX_WORDS = 10; //max words to print
+let counter = 0; //words printed so far
+
+
 const transformStream = Transform({
   transform(buffer, _, callback) {
     if (counter < MAX_WORDS){
