a70d9a327a65d30a44eb04c75893259c780ec436
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-02 13:48:46 -0500

    added usage message

--- word-search.js
+++ word-search.js
@@ -15,6 +15,11 @@ const searchRegExp = new RegExp(`^${searchTerm}`, 'i');
 //create the read stream
 const readStream = createReadStream("/usr/share/dict/words");
 
+if(process.argv.length === 2){
+  console.log("To search please enter: ./word-search.js [seachTerm]");
+  process.exit();
+}
+
 readStream.pipe(es.split('\n'))
 .pipe(es.map((datum, callback) => {
   if(datum.toString().search(searchRegExp) === 0){
