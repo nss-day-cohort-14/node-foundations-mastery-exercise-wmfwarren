dac5e0ebc7a0d38cd8bea127e6705cf629293f8e
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-09-02 13:42:45 -0500

    made logic more sound

--- word-search.js
+++ word-search.js
@@ -2,18 +2,22 @@
 
 'use strict'
 
+//eternal packages
 const {createReadStream} = require("fs");
 const {Transform} = require("stream");
 const es = require("event-stream")
 const limiter = require("./limit-ten.js");
+
 // get the cmd line arg to search
 const [,,searchTerm, ...rest] = process.argv;
 const searchRegExp = new RegExp(`^${searchTerm}`, 'i');
+
 //create the read stream
 const readStream = createReadStream("/usr/share/dict/words");
+
 readStream.pipe(es.split('\n'))
 .pipe(es.map((datum, callback) => {
-  if(datum.toString().search(searchRegExp) !== -1){
+  if(datum.toString().search(searchRegExp) === 0){
     callback (null, `${datum}\n`)
   } else {
    callback();
